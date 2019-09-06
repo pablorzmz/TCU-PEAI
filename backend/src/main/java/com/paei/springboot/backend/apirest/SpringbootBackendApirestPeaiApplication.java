b7a157b55185ccff36193885c377d1df842f7ec3
Pablo <jose.ramirez16@ucr.ac.cr>
2019-09-05 20:11:22 -0600

    Project set up

---
+++ backend/src/main/java/com/paei/springboot/backend/apirest/SpringbootBackendApirestPeaiApplication.java
@@ -0,0 +1,13 @@
+package com.paei.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApirestPeaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApirestPeaiApplication.class, args);
+    }
+
+}

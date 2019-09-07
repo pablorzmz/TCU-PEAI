bc3e21d3ef4f5fe7b8aeb78c645b667ce868d4d3
Jefferson Alvarez <jeffalvarez08@gmail.com>
2019-09-06 23:52:29 -0600

    Login inicial, ejemplo de directorios y jerarquia de modulos

--- frontend/src/app/modules/about/about.module.ts
+++ frontend/src/app/modules/about/about.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import {AboutComponent} from './pages/about/about.component';
-import {AboutRoutingModule} from './about-routing.module';
+import { AboutComponent } from './pages/about/about.component';
+import { AboutRoutingModule } from './about-routing.module';
 
 @NgModule({
   declarations: [AboutComponent],

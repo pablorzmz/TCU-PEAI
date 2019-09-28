d3cb7b5750be257764c20e86984675635dea1141
Jefferson Alvarez <jeffalvarez08@gmail.com>
2019-09-28 05:34:15 -0600

    Interfaz de presentacion de areas tematicas terminada pero falata definir el formato

--- frontend/src/app/modules/institucion/pages/listar-areas-tematicas/listar-areas-tematicas.component.ts
+++ frontend/src/app/modules/institucion/pages/listar-areas-tematicas/listar-areas-tematicas.component.ts
@@ -16,17 +16,19 @@ export class ListarAreasTematicasComponent implements OnInit {
   areasTematicas: Array<AreaTematica>;   // Contiene las áreas temáticas de institucón
 
   ngOnInit() {
-    const nombreInstitucion = this.institucion.getNombre();
+    // const nombreInstitucion = this.institucion.getNombre();
+    const nombreInstitucion = 'Cedes Don Bosco';
     // Se solicitan las areas tematicas de la institucion
     const request = this.institucionService.getAreasTematicas(nombreInstitucion).subscribe(
       res => {
         // Si las recibe se asignan al atributo areasTematicas
         this.areasTematicas = res;
-        console.log(res);
+        console.log(this.areasTematicas);
         // Quitamos la subscripcion
         request.unsubscribe();
       },
       // En caso de que no se reciba correctamente se lanza una excepcion
+      // Esto se implementa cuando esté el interceptor
       err => console.error(err)
     );
   }

d73fad3a5796b24f5703ef707fe9b5e53fcfde30
Walter <walter.bg7@gmail.com>
2019-10-01 19:20:55 -0600

    Borro un método y unos imports que no se necesitan

--- backend/src/main/java/com/paei/springboot/backend/apirest/dao/real/IInstitucionDao.java
+++ backend/src/main/java/com/paei/springboot/backend/apirest/dao/real/IInstitucionDao.java
@@ -3,17 +3,6 @@ package com.paei.springboot.backend.apirest.dao.real;
 import com.paei.springboot.backend.apirest.model.entity.real.Institucion;
 import com.paei.springboot.backend.apirest.model.entity.real.InstitucionPK;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.paei.springboot.backend.apirest.model.entity.real.AreaTematica;
-import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 
 public interface IInstitucionDao extends JpaRepository<Institucion, InstitucionPK> {
-    /**
-     * Consulta las áreas temáticas de una institución
-     * @param institucionPK es la pk de la institución de la cuál se desean obtener las areas temáticas
-     * @return Retorna una lista con las áreas temáticas de una institución
-     */
-    @Query("SELECT AreaTematica FROM Institucion I JOIN I.areaTematicas AreaTematica WHERE I.institucionPK = ?1")
-    List<AreaTematica> findAreaTematicaByInstitucion(InstitucionPK institucionPK);
-
 }

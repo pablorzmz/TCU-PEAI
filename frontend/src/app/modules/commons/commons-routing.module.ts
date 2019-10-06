9b7bb501c9a668fa66fca41aca42310ec93ee714
Walter <walter.bg7@gmail.com>
2019-10-06 13:39:15 -0600

    Borro cosas que no se ocupan

--- frontend/src/app/modules/commons/commons-routing.module.ts
+++ frontend/src/app/modules/commons/commons-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AuthGuardService} from '../../core/guard/auth.guard';
-import {ListarCursosComponent} from '../cursos/pages/listar-cursos/listar-cursos.component';
-import {InstitucionesComponent} from '../instituciones/pages/listar-instituciones/instituciones.component';
 
 const routes: Routes = [
-  {
-    path: 'instituciones/page/:page',
-    component: InstitucionesComponent,
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'cursos/page/:page',
-    component: ListarCursosComponent,
-    canActivate: [AuthGuardService]
-  }
 ];
 
 @NgModule({

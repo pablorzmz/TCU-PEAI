8ab5c33291d22b0a3e135bd6436058d7634fb8c7
pablorzmz <pablow75@gmail.com>
2019-10-07 18:05:28 -0600

    Fixes a curso

--- backend/src/main/java/com/paei/springboot/backend/apirest/exceptions/VistaPrincipalCursoNotFoundException.java
+++ backend/src/main/java/com/paei/springboot/backend/apirest/exceptions/VistaPrincipalCursoNotFoundException.java
@@ -2,6 +2,6 @@ package com.paei.springboot.backend.apirest.exceptions;
 
 public class VistaPrincipalCursoNotFoundException extends RuntimeException {
     public VistaPrincipalCursoNotFoundException(){
-        super("La vista para el curso no se encuentra en el sistema.");
+        super("La vista para el curso seleccionado no está disponible.");
     }
 }

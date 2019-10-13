4703e7033281ac532d45a613035402a366f60791
pablorzmz <pablow75@gmail.com>
2019-10-12 19:12:38 -0600

    listo listar y futuro agregar subsecciones de material

---
+++ frontend/src/app/modules/grupo/components/agregar-subseccion-material-grupo/agregar-subseccion-material-grupo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-subseccion-material-grupo',
+  templateUrl: './agregar-subseccion-material-grupo.component.html',
+  styleUrls: ['./agregar-subseccion-material-grupo.component.css']
+})
+export class AgregarSubseccionMaterialGrupoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

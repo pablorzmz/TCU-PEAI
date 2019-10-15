c519c4365bc41f218769fce341275d5832771a45
pablorzmz <pablow75@gmail.com>
2019-10-14 23:08:52 -0600

    Dao, servicio e implementacion para MaterialService

--- backend/src/main/java/com/paei/springboot/backend/apirest/controllers/real/MaterialController.java
+++ backend/src/main/java/com/paei/springboot/backend/apirest/controllers/real/MaterialController.java
@@ -1,14 +1,33 @@
 package com.paei.springboot.backend.apirest.controllers.real;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.paei.springboot.backend.apirest.model.entity.real.Grupo;
+import com.paei.springboot.backend.apirest.services.real.IMaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 
 @RequestMapping("/api/material")
 public class MaterialController {
+
+    @Autowired
+    private IMaterialService iMaterialService;
+
+    /**
+     * Método que permite recuperar todos lo materiales de un grupo por cada una de sus subsecciones de material
+     * @param grupo El grupo como tal para recuperar los materiales
+     * @param result El resultado del binding para ver si el grupo es válido
+     * @return Un response entity con los datos, o bien, las respecticas excepciones.
+     */
+    @GetMapping("/materiales_grupo")
+    public ResponseEntity<?> obtenerMaterialesGrupoPorSubseccion(@Valid @RequestBody Grupo grupo,  BindingResult result){
+        return null;
+    }
 
 }
